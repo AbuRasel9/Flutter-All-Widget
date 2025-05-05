d920fa36299ff8a2f5623e92f5be9eb5ff1ce405
PTSL-RASEL <aburaselahamed99@gmail.com>
2025-05-05 17:26:01 +0600

    bloc create started

--- lib/apiCall/animal_app/bloc/animal_data_event.dart
+++ lib/apiCall/animal_app/bloc/animal_data_event.dart
@@ -1,2 +1,14 @@
-import 'e';
-class AnimalDataEvent extends Equ
+
+import 'package:equatable/equatable.dart';
+
+abstract class AnimalDataEvent extends Equatable{
+  const AnimalDataEvent();
+@override
+  List<Object?> get props => [];
+}
+class FetchAnimalData extends AnimalDataEvent{
+  final String countryName;
+  const FetchAnimalData({required this.countryName});
+  @override
+  List<Object?> get props => [countryName];
+}
